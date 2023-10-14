e3decf2621352043cbf39fd4b95e66d739ba91be
vaishnavi-trial <vaishnavigv@Ubuntu.myguest.virtualbox.org>
2023-10-15 00:53:15 +0530

    initial backend sql code commit

---
+++ backend/DDL.sql
@@ -0,0 +1,125 @@
+create database Capstone_Mapping;
+use Capstone_Mapping;
+
+/*  numeric keys make for faster lookup  */
+
+/*
+
+make a
+
+*/
+
+create table Student (
+    srn char(13) primary key,
+    semester int not null check (semester in (1,2,3,4,5,6,7,8)),
+    cgpa float(2) check ((cgpa >=0) and (cgpa <=10)),
+    Fname varchar(15) not null,
+    Lname varchar(15) not null,
+    email varchar(50) not null,
+    resume_doc mediumblob default null,
+    pswd varchar(20) not null,
+    outgoing_year year
+);
+
+create table Teacher (
+    teacher_id char(13) primary key,
+    Fname varchar(15) not null,
+    Lname varchar(15) not null,
+    email varchar(50) not null,
+    floor int default null,
+    cabin_no varchar(10) default null,
+    pswd varchar(20) not null
+);
+
+create table Team (
+    team_id char(10) primary key
+);
+
+alter table Student add team_id char(10);
+alter table Student add foreign key (team_id) references Team(team_id) on update cascade on delete set null;
+
+/* 
+
+when the supervisor hits limit, automatically reject the pending requests 
+if active_projects = team_limit-1 and supervisor wants to accept the last time,
+need to put pop saying that this is the last team they can accept, and on accepting the request
+
+
+*/
+
+create table Supervisor (
+    supervisor_id char(13) primary key,
+    team_limit int not null check (team_limit > 0),
+    active_projects int not null check (active_projects >= 0),
+    foreign key (supervisor_id) references Teacher(teacher_id) on update cascade on delete restrict
+);
+
+    /* need to find a way to be able to set domain values */
+
+create table Supervisor_years (
+    supervisor_id char(13),
+    batch year
+)
+
+create table Supervisor_Domains (
+    supervisor_id char(13) not null,
+    domain varchar(30) not null check (domain in ('Natural Language Processing', 'Machine Learning and Arrificial Intelligence')),
+    constraint domain_pk primary key (supervisor_id, domain),
+    foreign key (supervisor_id) references Supervisor(supervisor_id) on update cascade on delete cascade
+);
+
+/* for a supervisor id being 0000000000000, return value 'this supervisor is no longer available' */
+
+create table Request (
+    request_id char(10) primary key,
+    team_id char(10) not null,
+    supervisor_id char(13) not null default '0000000000000',
+    interested_domain varchar(30) not null,
+    idea varchar(140),
+    constraint team_id_ref foreign key (team_id) references Team(team_id) 
+            on update cascade on delete cascade,
+    constraint super_id_ref foreign key (supervisor_id) references Supervisor(supervisor_id)
+            on update cascade on delete set default
+);
+
+/* an accepted request from the student's side should be used to create the project entry*/
+
+create table Project (
+    project_id char(10) primary key,
+    team_id char(10) not null,
+    supervisor_id char(13) not null,
+    start_d date,
+    end_d date,
+    cur_phase int not null check(cur_phase in(1,2,3)),
+    domain varchar(30) not null,
+    problem_statement varchar(140) not null,
+    foreign key(team_id) references Team(team_id),
+    foreign key(supervisor_id) references Supervisor(supervisor_id)
+);
+
+create table Review (
+    project_id char(10),
+    phase int check (phase in (1,2,3)),
+    grade char(1) check (grade in ('S', 'A', 'B', 'C', 'D', 'E', 'F')),
+    primary key (project_id, phase),
+    foreign key (project_id) references Project(project_id)
+);
+
+create table Reviewed_by (
+    project_id char(10),
+    phase int check(phase in (1,2,3)),
+    reviewer_id char(13) not null,
+    feedback varchar(100) not null,
+    primary key(project_id, phase, reviewer_id),
+    foreign key(project_id) references Project(project_id),
+    foreign key(reviewer_id) references Teacher(teacher_id)
+);
+
+create table Document (
+    project_id char(10),
+    phase int check(phase in (1,2,3)),
+    dname varchar(20) not null,
+    dfile blob not null,
+    primary key(project_id, phase),
+    foreign key (project_id) references Project(project_id)
+);
