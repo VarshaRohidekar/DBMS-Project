bd36426f7e53731e77bd510f6ac2745548213782
Varsha Rohidekar <varsharohidekar@192.168.1.15>
2023-10-18 20:32:56 +0530

    Admin,teacher and student level with auth

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 import mysql.connector
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'dbms'
 
 # A simple dictionary to store user data (replace with a proper database)
 
 users = {
-    'user1': {'password': 'password1', 'name': 'John Doe'},
-    'user2': {'password': 'password2', 'name': 'Jane Smith'},
+    'user1': {'password': 'password1', 'name': 'John Doe','auth':1},
+    'user2': {'password': 'password2', 'name': 'Jane Smith','auth':2},
+    'user3': {'password': 'password3', 'name': 'ADMIN CHECKING','auth':3}
 }
 
 logged_in_users = set()
@@ -28,24 +30,32 @@ def home():
     return render_template('home.html', data=data, column_names=column_names)
 
 logged_in_users = set()
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['POST','GET'])
 def login():
-    if request.method == 'POST':
+    if request.method=='POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        # Dropdown box -> teacher,student,admin -> pass it to db and get true/false
         if username in users and users[username]['password'] == password:
-            if username in logged_in_users:
-                return redirect(url_for('profile', username=username))
-            logged_in_users.add(username)
-            return redirect(url_for('profile', username=username))
+            # Successful login
+            session['username'] = username  # Store the username in the session
+            auth_level = users[username]['auth']
+            if auth_level == 1:
+                return redirect(url_for('teacherprofile', username=username))
+            elif auth_level==2:
+                # The user is a student
+                return redirect(url_for('studentprofile', username=username))
+            elif auth_level==3:
+                return redirect(url_for('adminprofile', username=username))
         else:
-            return "Login failed. Please check your credentials."
+            # Invalid username or password
+            return "Invalid credentials. Please try again."
     return render_template('login.html')
+
 
 # Check whether the person logging in is a teacher/student or admin
 @app.route('/studentprofile/<username>', methods=['GET', 'POST'])
-def profile(username):
+def studentprofile(username):
     user = users.get(username)
     if not user:
         return "User not found."
@@ -61,6 +71,19 @@ def profile(username):
         return "Profile updated successfully."
     return render_template('studentprofile.html', username=username, user=user)
 
+
+@app.route('/teacherprofile/<username>', methods=['GET', 'POST'])
+def teacherprofile(username):
+    user = users.get(username)
+    if not user:
+        return "User not found."
+    if request.method == 'POST':
+        # Add teacher-specific logic for updating the profile
+        user['email'] = request.form.get('email')
+        user['bio'] = request.form.get('bio')
+        return "Teacher profile updated successfully."
+    return render_template('teacherprofile.html', username=username, user=user)
+
 ''' TEACHER PROFILE PAGE '''
 # If a teacher is a supervisor -> modify teacher page to supervisor
 # TEACHER PAGE -> Teacher ID, Name, GET REVIEWS(SEE LATER)
@@ -72,7 +95,17 @@ def profile(username):
 # View Active Teams(button) -> Each team will have "view project" button,List of active project teams with team name,project name,BATCH
 
 '''ADMIN PAGE'''
-
+@app.route('/adminprofile/<username>', methods=['GET', 'POST'])
+def adminprofile(username):
+    user = users.get(username)
+    if not user:
+        return "User not found."
+    if request.method == 'POST':
+        # Add teacher-specific logic for updating the profile
+        user['email'] = request.form.get('email')
+        user['bio'] = request.form.get('bio')
+        return "Teacher profile updated successfully."
+    return render_template('adminprofile.html', username=username, user=user)
 
 
 @app.route('/logout')
