0be80db279b3d2469eea15eda9c103b22a9e252b
Vaishnavi <vaishnavigv.sn@gmail.com>
2023-10-18 20:51:50 +0530

    Student Dashboard Functions and Login Page Functions initial commit

---
+++ backend/LoginPageFunc.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from mysql.connector import errorcode 
+import config    
+    
+def verify_login(role, id, password):
+
+    try:
+        cnx = mysql.connector.connect(**config.config)
+
+    # need to find a better way to handle errors
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        print("connected to", cnx._database)        #cnx._database -- value of current database
+
+    cnx_cursor = cnx.cursor()
+    
+    if role == 's':
+        result = cnx_cursor.execute(""" SELECT srn, pswd from Student where srn = %(srn)s""", {'srn' : id})
+    elif role == 't':
+        result = cnx_cursor.execute(""" SELECT teacher_id, pswd from Teacher where teacher_id = %(teacher_id)s""", {'teacher_id' : id})
+    content = cnx_cursor.fetchone()
+    cnx.close()
+    
+    if content is None:
+        return "ID not found"
+    
+    else:
+        (s,p) = content
+        if p == password:
+            return True 
+        else:
+            return False
+
+
+# x = verify_login('t', "T1234ASDFGHJ3", "1234ASDFGHKL")
+# print(x)
---
+++ backend/StudentDashboardFunc.py
@@ -0,0 +1,49 @@
+import mysql.connector
+from mysql.connector import errorcode 
+import config
+
+
+# returning hasTeam and hasResume values in the get_student_details function itself
+
+def get_student_details(srn):
+    
+    try:
+        cnx = mysql.connector.connect(**config.config)
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        print("connected to", cnx._database)        #cnx._database -- value of current database
+        
+    cnx_cursor = cnx.cursor()
+    
+    result = cnx_cursor.execute("""SELECT Fname, Lname, email, outgoing_year, cgpa, semester, team_id, resume_doc 
+                                   FROM Student where srn = %(srn)s""", {'srn' : srn})
+    (first_name, last_name, email, outgoing_year, cgpa, semester, team_id, resume_doc) = cnx_cursor.fetchone()
+
+    cnx.close()
+    
+    teamEligibility = True
+    hasTeam = False
+    hasResume = False
+    if team_id is not None:
+        hasTeam = True
+        
+    if resume_doc is not None:
+        hasResume = True
+    
+    if int(semester) < 5:
+        teamEligibility = False
+        
+    
+    return (first_name, last_name, email, outgoing_year, cgpa, semester, teamEligibility, hasTeam, hasResume)
+
+
+# x = get_student_details("PES1UG04MVE50")
+# for i in x:
+#     print(i)
